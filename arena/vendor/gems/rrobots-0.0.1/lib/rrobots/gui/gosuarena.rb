048d733f8e6334eede1a4b29f6debc073508fd22
Daniel Fone <daniel@fone.net.nz>
2014-04-16 23:49:55 +1200

    Score driven leaderboard

--- arena/vendor/gems/rrobots-0.0.1/lib/rrobots/gui/gosuarena.rb
+++ arena/vendor/gems/rrobots-0.0.1/lib/rrobots/gui/gosuarena.rb
@@ -2,9 +2,9 @@ require 'gosu'
 
 BIG_FONT = 'Courier New'
 SMALL_FONT = 'Courier New'
-COLORS = ['white', 'blue', 'yellow', 'red', 'lime'] 
-FONT_COLORS = [0xffffffff, 0xff0008ff, 0xfffff706, 0xffff0613, 0xff00ff04]
-    
+COLORS = ['white', 'blue', 'yellow', 'red', 'lime']
+FONT_COLORS = [0xffffffff, 0xff4444ff, 0xfffff706, 0xffff0613, 0xff00ff04]
+
 GosuRobot = Struct.new(:body, :gun, :radar, :speech, :info, :status, :color, :font_color)
 
 module ZOrder
@@ -14,7 +14,7 @@ end
 class RRobotsGameWindow < Gosu::Window
   attr_reader :battlefield, :xres, :yres
   attr_accessor :on_game_over_handlers, :boom, :robots, :bullets, :explosions
-  
+
   def initialize(battlefield, xres, yres)
     super(xres, yres, false, 16)
     self.caption = 'RRobots'
@@ -32,14 +32,14 @@ class RRobotsGameWindow < Gosu::Window
   def on_game_over(&block)
     @on_game_over_handlers << block
   end
-  
+
   def init_window
     @boom = (0..14).map do |i|
       Gosu::Image.new(self, File.join(File.dirname(__FILE__),"../images/explosion#{i.to_s.rjust(2, '0')}.bmp"))
     end
     @bullet_image = Gosu::Image.new(self, File.join(File.dirname(__FILE__),"../images/bullet.png"))
   end
-  
+
   def init_simulation
     @robots, @bullets, @explosions = {}, {}, {}
   end
@@ -58,15 +58,15 @@ class RRobotsGameWindow < Gosu::Window
     draw_bullets
     draw_explosions
   end
-  
+
   def simulate(ticks=1)
     @explosions.reject!{|e,tko| e.dead }
     @bullets.reject!{|b,tko| b.dead }
-    @robots.reject! { |ai,tko| ai.dead}
+    #@robots.reject! { |ai,tko| ai.dead}
     ticks.times do
       if @battlefield.game_over
         @on_game_over_handlers.each{|h| h.call(@battlefield) }
-          winner = @robots.keys.first
+          winner = @robots.keys.sort_by { |r| -1*r.score }.first
           whohaswon = if winner.nil?
             "Draw!"
           elsif @battlefield.teams.all?{|k,t|t.size<2}
@@ -80,7 +80,7 @@ class RRobotsGameWindow < Gosu::Window
       @battlefield.tick
     end
   end
-  
+
   def draw_robots
     @battlefield.robots.each_with_index do |ai, i|
       next if ai.dead
@@ -99,7 +99,7 @@ class RRobotsGameWindow < Gosu::Window
       @robots[ai].body.draw_rot(ai.x / 2, ai.y / 2, ZOrder::Robot, (-(ai.heading-90)) % 360)
       @robots[ai].gun.draw_rot(ai.x / 2, ai.y / 2, ZOrder::Robot, (-(ai.gun_heading-90)) % 360)
       @robots[ai].radar.draw_rot(ai.x / 2, ai.y / 2, ZOrder::Robot, (-(ai.radar_heading-90)) % 360)
-      
+
       @robots[ai].speech.draw_rel(ai.speech.to_s, ai.x / 2, ai.y / 2 - 40, ZOrder::UI, 0.5, 0.5, 1, 1, font_col)
       @robots[ai].info.draw_rel("#{ai.name}", ai.x / 2, ai.y / 2 + 30, ZOrder::UI, 0.5, 0.5, 1, 1, font_col)
       @robots[ai].info.draw_rel("#{ai.energy.to_i}", ai.x / 2, ai.y / 2 + 50, ZOrder::UI, 0.5, 0.5, 1, 1, font_col)
--- arena/vendor/gems/rrobots-0.0.1/lib/rrobots/gui/leaderboard.rb
+++ arena/vendor/gems/rrobots-0.0.1/lib/rrobots/gui/leaderboard.rb
@@ -1,18 +1,20 @@
 class Leaderboard
   def initialize(window, robots)
-    @font_size = 24 
+    @font_size = 24
     @robots = robots
-    @font = Gosu::Font.new(window, 'Courier New', @font_size) 
+    @font = Gosu::Font.new(window, 'Courier New', @font_size)
     @x_offset = @font_size
     @y_offset = @font_size * 2
   end
 
   def draw
     if @robots
-      @robots.each_with_index do |r, i|
+      @robots.sort_by { |r| -1*r.first.score }.each_with_index do |r, i|
         y = @y_offset + i * @font_size
-        @font.draw("#{r.first.name}", @x_offset, y, ZOrder::UI, 1.0, 1.0, r.last.font_color)
-        @font.draw("#{r.first.energy.to_i}", @x_offset + (@font_size * 6), y, ZOrder::UI, 1.0, 1.0, r.last.font_color)
+        color = r.last.font_color
+        color &= 0x55ffffff if r.first.dead
+        @font.draw("#{r.first.name}", @x_offset, y, ZOrder::UI, 1.0, 1.0, color)
+        @font.draw('%.1f' % r.first.score, @x_offset + (@font_size * 6), y, ZOrder::UI, 1.0, 1.0, color)
       end
     end
   end
